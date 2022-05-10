37ece32b6d5ddb2821bac464eb69d5cfb5cb3413
LongTuan-123 <85407964+LongTuan-123@users.noreply.github.com>
2022-05-11 00:02:24 +0700

    update

--- src/components/Userprofile/TicketList.js
+++ src/components/Userprofile/TicketList.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Navigation from "../../Layout/Navigation";
+
 import { API_TICKET_USER_ID } from "../../config/endpointapi";
 import { getToken } from "../../Http";
 import Layout from "../../Layout/Layout";
-import '../../style/Ticket.scss'
+
+import "../../style/Ticket.scss";
 
 const TicketList = () => {
   const user = localStorage.getItem("data_user");
@@ -25,39 +28,36 @@ const TicketList = () => {
       getTicketList();
     }
   }, [user]);
-  
+console.log(user.first_name);
   return (
     <Layout>
+      <Navigation>
+        Lịch sử giao dịch
+      </Navigation>
+      <div className="layout">
+
       <div className="layout-ticket">
         {listTicket.map((li) => (
           <div className="ticket">
             <img src="https://thumbs.dreamstime.com/b/cinema-ticket-icon-retro-template-isolated-white-background-set-tickets-to-theater-concert-other-event-vector-173751967.jpg" />
             <div className="ticket-content">
-              <div>
-                ID vé: {li?.id}
-              </div>
-              <div>
-                Phim: {li?.showtime?.movie_id}
-              </div>
-              <div>
-                Chỗ ngồi: {li?.seat?.row}{li?.seat?.order}
-              </div>
+              <div>ID vé: {li?.id}</div>
               <div>
                 Họ và tên: {li?.user?.first_name} {li?.user?.last_name}
               </div>
+              <div>Phim: {li?.showtime?.movie_id}</div>
               <div>
-                Ngày chiếu: {li?.showtime?.show_date}
+                Chỗ ngồi: {li?.seat?.row}
+                {li?.seat?.order}
               </div>
-              <div>
-                Giờ chiếu: {li?.showtime?.show_time}
-              </div>
-              <div>
-                Tiền: {li?.money}
-              </div>
-
+           
+              <div>Ngày chiếu: {li?.showtime?.show_date}</div>
+              <div>Giờ chiếu: {li?.showtime?.show_time}</div>
+              <div>Tiền: {li?.money} $</div>
             </div>
           </div>
         ))}
+      </div>
       </div>
     </Layout>
   );
